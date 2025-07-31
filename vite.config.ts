fd46d37e5abf7347bfea2076df6334ba4defb079
n0tShayan <shayanjan555@gmail.com>
2025-07-31 12:10:06 +0500

    change script

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: /"shayan-portfolio"/,
+  base: "/shayan-portfolio/",
   server: {
     host: "::",
     port: 8080,
